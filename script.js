306377c09b751db614e0cc173f5e23bbea0d2917
BrianPizz <bpizzimentijr@outlook.com>
2023-09-03 14:05:27 -0400

    added character variables

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+const number = "0123456789"
+const symbol = "`!@#$%^&*()_+=-[];,./<>?"
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
