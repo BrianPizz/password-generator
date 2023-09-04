4b186bb7a67140df165d180b277a395c7222f368
BrianPizz <bpizzimentijr@outlook.com>
2023-09-04 13:33:31 -0400

    added prompts for password criteria

--- script.js
+++ script.js
@@ -5,15 +5,29 @@ const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const lowerCase = "abcdefghijklmnopqrstuvwxyz"
 const number = "0123456789"
 const symbol = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+function generatePassword(){
 
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  
+  
   passwordText.value = password;
 
+  
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
+generateBtn.onclick = function (){
+  window.confirm("Would you like to use uppercase characters?");
+  window.confirm("Would you like to use lowercase characters?");
+  window.confirm("Would you like to use numerical characters?");
+  window.confirm("Would you like to use special characters?");
+  window.prompt("How long would you like your password to be")
+}
+
+
